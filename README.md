5f403a172384f278457f467fc58c1a763d5e50be
eshook <eshook@gmail.com>
2020-07-22 11:00:06 -0500

    Building first template lesson file
    Creating a template lesson file for all Hour of CI lessons that
    incorporates all necessary code to initialize, track, and present the
    materials and interactive components.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # lessons-dev
 This is a working repository for the development and refinement of Hour of CI lessons. Lessons in this repository will likely be partially broken.
+
+To load this repository using the Hour of CI environment you should use the following URL:
+
+http://pilot.hourofci.org/hub/user-redirect/git-pull?repo=https%3A%2F%2Fgithub.com%2Fhourofci%2Flessons-dev&app=notebook
+  
+This will automatically pull the lessons in development and make them available on Hour of CI in the 'lessons-dev' folder.  
+
+## Adding a new notebook
+
+To add a new Jupyter Notebook to a lesson:
+
+  1. Copy the template.ipynb file from the 'template' folder. It contains several special cells and additional header information that is useful for Hour of CI lesson notebooks.
+  
+  2. Figure out the path and name of the new notebook. For example if you are creating the fifth spatial-thinking notebook a path may be:
+  
+    spatial-thinking/spatial-thinking-5.ipynb
+	
+	Use this path to enable Hour of CI to trust this new notebook. We do this by adding it to the 'postBuild' file.
+	
+	jupyter trust spatial-thinking/spatial-thinking-5.ipynb
+	
+	
+
+Make sure to test the material using a small screen size:
+
+In Chrome you can use 'Window Resizer' to set different sizes.
+The target size is a 11-13" netbook.
+https://chrome.google.com/webstore/detail/window-resizer/kkelicaakdanhinjdeammmilcgefonfh?hl=en
---
+++ supplementary/hourofci.py
@@ -0,0 +1,162 @@
+import ipywidgets as widgets
+import requests
+# Retrieve username
+import getpass
+# Encoding
+import hashlib
+
+# Execute a notebook
+import io
+import nbformat 
+from IPython import get_ipython
+
+# v7 - Add user_agent parameter, which is from the notebook
+def SubmitBtn(user_agent,lesson,lesson_level,question,widget):
+    """ Display a submit button
+
+    Input:
+    lesson (e.g., "geospatial-data")
+    lesson_level (e.g., "beginner")
+    question - defined behind instead of in the notebooks
+    widget - the widget of which value will be submitted
+    """
+
+    # Submit button
+    button = widgets.Button(
+        description = 'Submit',
+        disabled = False,
+        button_style = '',
+        icon = 'check'
+    )
+    
+    display(button)
+    
+    # Output
+    output = widgets.Output()
+    display(output) 
+    
+
+    # Submit function
+    def submit(b):
+
+        # Logging
+        host = "check.hourofci.org"
+        port = "4000" 
+        answer = widget.value
+
+        # v6 - Retrieve username
+        # username = str(getpass.getuser())
+        #username = str(getpass.getuser()).split('-')[1] # In Jupyterhub, getuser() = Jupyter-username
+		username = str(getpass.getuser())
+        # v7 - Encode username
+        username_hash = hashlib.md5(username.encode()).hexdigest()
+
+        # v7 - Encode user agent
+        user_agent_hash = hashlib.md5(user_agent.encode()).hexdigest()
+
+        # v6 - Add username 
+        # v7 - Add user agent
+        url = "https://{}:{}/{}/{}/{}/{}/{}/{}".format(host, port, username_hash, user_agent_hash, lesson, lesson_level, question, answer)
+        # print(url)
+        # Send_request
+        r = requests.get(url)
+
+        # Print widget value
+        with output:
+            output.clear_output()
+            print(widget.value)
+            if r.status_code == requests.codes.ok:
+                print("Submit Successfully!")
+    
+    button.on_click(submit)
+
+
+
+# v8 - Add a run button to run a cell and record this action
+def RunBtn(user_agent,lesson,lesson_level,nbfilename,code_tag):
+    """ Display a run button
+
+    Input:
+    lesson (e.g., "geospatial-data")
+    lesson_level (e.g., "beginner")
+    nbfilename - name of the current jupyter notebook
+    code_tag - tags of the code cell we want to run (e.g., "Q1")
+    """
+
+    # Run button
+    button = widgets.Button(
+        description = '   Run',
+        disabled = False,
+        button_style = '',
+        icon = 'step-forward'
+    )
+    
+    display(button)
+    
+    # Output
+    output = widgets.Output()
+    display(output) 
+    
+    # Run function
+    def run(b):
+        output.clear_output()
+
+        # v8 - Run a cell
+        # TODO: add the path of the file
+        nbfile = nbfilename+".ipynb"
+        execute_notebook_cell(nbfile,code_tag)
+
+        # Logging
+        host = "check.hourofci.org"
+        port = "4000" 
+
+        # Retrieve username
+        username = str(getpass.getuser())
+        # username = str(getpass.getuser()).split('-')[1] # In Jupyterhub, getuser() = Jupyter-username
+        # Encode username
+        username_hash = hashlib.md5(username.encode()).hexdigest()
+
+        # Encode user agent
+        user_agent_hash = hashlib.md5(user_agent.encode()).hexdigest()
+
+        url = "https://{}:{}/{}/{}/{}/{}/{}/{}".format(host, port, username_hash, user_agent_hash, lesson, lesson_level, code_tag, "Run")
+        # Send_request
+        r = requests.get(url)
+        # print(r.status_code)
+
+        # Give feedback if run sucessfully
+        with output:
+            # output.clear_output()
+            if r.status_code == requests.codes.ok:
+                print("\n\nRun Successfully!")
+
+    def execute_notebook_cell(nbfile,code_tag):
+        """ Execute the specific cells in a notebook
+        """
+        with io.open(nbfile) as f:
+            nb = nbformat.read(f, 4)      
+        
+        ip = get_ipython()
+    
+        # The results are shown in the output area so they can be cleared
+        with output:
+            for cell in nb.cells:
+                if 'tags' in cell.metadata and code_tag in cell.metadata.tags:
+                    ip.run_cell(cell.source)
+    
+    button.on_click(run)
+
+
+# def execute_notebook_cell(nbfile,code_tag):
+#     """ Execute the specific cells in a notebook
+#     """
+#     with io.open(nbfile) as f:
+#         nb = nbformat.read(f, 4)      
+        
+#     ip = get_ipython()
+    
+#     for cell in nb.cells:
+#         if 'tags' in cell.metadata and code_tag in cell.metadata.tags:
+#             ip.run_cell(cell.source)
+
+
