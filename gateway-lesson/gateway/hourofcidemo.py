07cbb41bafa7ac4301a0628a46c53d7dd4fc9630
eshook <eshook@gmail.com>
2020-08-14 17:13:04 -0500

    Adding initial Gateway draft.

---
+++ gateway-lesson/gateway/hourofcidemo.py
@@ -0,0 +1,11 @@
+def readimage(filename):
+    print("Reading image", filename)
+    return "this thing"
+
+def ndvi(image):
+    print("Calculating NDVI")
+    if "this thing" == image:
+        # Do something here.
+        print("Displaying NDVI")
+        return "Calculated NDVI."
+    return "Nope"
