7f432f9d60539f5b5807b73071c31c944f1cf3b4
IrisZhiweiYan <irisgogo.yan@gmail.com>
2020-09-21 09:00:50 -0500

    Added logging in Gateway

--- supplementary/hourofci.py
+++ supplementary/hourofci.py
@@ -1,4 +1,5 @@
 import ipywidgets as widgets
+from ipywidgets import Layout
 import requests
 # Retrieve username
 import getpass
@@ -14,7 +15,7 @@ def out():
     pass
 
 # Add user_agent parameter, which is from the notebook
-def SubmitBtn(user_agent, lesson, lesson_level, question,widget,out_func=out):
+def SubmitBtn(user_agent, lesson, lesson_level, question,widget,out_func=out,text="Submit"):
     """ Display a submit button
 
     Input:
@@ -22,12 +23,14 @@ def SubmitBtn(user_agent, lesson, lesson_level, question,widget,out_func=out):
     lesson_level (e.g., "beginner")
     question - defined behind instead of in the notebooks
     widget - the widget of which value will be submitted
-    out_func - printing after the button is clicked
+    out_func - printing after the button is clicked (default value is an empty function)
+    text - description that shows on the button (default value is "Submit")
     """
 
     # Submit button
     button = widgets.Button(
-        description = 'Submit',
+        description = text,
+        layout=Layout(width='auto', height='auto'),
         disabled = False,
         button_style = '',
         icon = 'check'
@@ -38,6 +41,7 @@ def SubmitBtn(user_agent, lesson, lesson_level, question,widget,out_func=out):
     # Output
     output = widgets.Output()
     display(output) 
+    
 
     # Submit function
     def submit(b):
@@ -45,8 +49,16 @@ def SubmitBtn(user_agent, lesson, lesson_level, question,widget,out_func=out):
         # Logging
         host = "check.hourofci.org"
         port = "4000" 
-        answer = widget.value
-
+        
+        if type(widget) == str: # If a string is passed, this string will be logged as the answer
+            answer = widget
+        elif type(widget) == list: # If there are multiple widgets, record them in a list
+            answer = []
+            for i in range(len(widget)):
+                answer.append(widget[i].value)
+        else:
+            answer = widget.value
+        
         # Retrieve username
         if str(getpass.getuser()).split('-')[0] == "jupyter":
             username = str(getpass.getuser()).split('-')[1] # In Jupyterhub, getuser() = Jupyter-username
@@ -60,7 +72,7 @@ def SubmitBtn(user_agent, lesson, lesson_level, question,widget,out_func=out):
 
         # Add username 
         # Add user agent
-        url = "https://{}:{}/{}/{}/{}/{}/{}/{}".format(host, port, username_hash, user_agent_hash, lesson, lesson_level, question, answer)
+        url = "https://{}:{}/{}/{}/{}/{}/{}/{}".format(host, port, username_hash, user_agent_hash, lesson, lesson_level, question, str(answer))
         # print(url)
         # Send_request
         r = requests.get(url)
@@ -68,7 +80,11 @@ def SubmitBtn(user_agent, lesson, lesson_level, question,widget,out_func=out):
         # Print widget value
         with output:
             output.clear_output()
-            print("Your answer is: "+widget.value)
+            
+            if type(widget) != str:
+                # print("Your answer is: " + str(answer))
+                pass 
+                
             if r.status_code == requests.codes.ok:
                 print("Submit Successfully!\n")
             out_func()
