614b8b18598a1574d965c35904394bf495a794ad
Mohsen Ahmadkhani <ahmad178@umn.edu>
2022-01-17 19:45:11 -0600

    all lessons tested including SMA

--- supplementary/hourofci.py
+++ supplementary/hourofci.py
@@ -1,17 +1,19 @@
 import ipywidgets as widgets
 from ipywidgets import Layout
 import requests
+from IPython.display import display, clear_output
 # Encoding
 import hashlib
 
 # Default response of a question
-# def out():
-#     print("Answer successfully submitted.\n")
 def out():
-    pass
+    print("Answer successfully submitted.\n")
+# def out():
+#     pass
 
 # Add user_agent parameter, which is from the notebook
-def SubmitBtn(user_agent, lesson, lesson_level, question,widget,out_func=out,text="Submit",test=False):
+# def SubmitBtn(user_agent, lesson, lesson_level, question,widget,out_func=out,text="Submit",test=False):
+def SubmitBtn(widget,out_func=out,text="Submit",test=False):
     """ Display a submit button
 
     Input:
@@ -41,42 +43,50 @@ def SubmitBtn(user_agent, lesson, lesson_level, question,widget,out_func=out,tex
 
     # Submit function
     def submit(b):
-
-        # Logging
-        host = "check.hourofci.org"
-        port = "4000" 
-        
-        if type(widget) == str: # If a string is passed, this string will be logged as the answer
-            answer = widget
-        elif type(widget) == list: # If there are multiple widgets, record them in a list
-            answer = []
-            for i in range(len(widget)):
-                answer.append(widget[i].value)
-        else:
-            answer = widget.value
-
-        # Encode user agent
-        user_agent_hash = hashlib.md5(user_agent.encode()).hexdigest()
-        
-        if test:
-            url = "https://{}:{}/test/{}/{}/{}/{}/{}".format(host, port, user_agent_hash, lesson, lesson_level, question, str(answer))
-        else:
-            url = "https://{}:{}/{}/{}/{}/{}/{}".format(host, port, user_agent_hash, lesson, lesson_level, question, str(answer))
-            
-        # Send_request
-        r = requests.get(url)
-
-        # Print widget value
         with output:
             output.clear_output()
-            
-            if type(widget) != str:
-                # print("Your answer is: " + str(answer))
-                pass 
-            
-            if r.status_code == requests.codes.ok:
-                print("Answer successfully submitted.\n")
-                
             out_func()
+#             print("Answer successfully submitted.\n")
+        
+        
+#     def submit_old(b):
+
+#         # Logging
+#         host = "check.hourofci.org"
+#         port = "4000" 
+        
+#         if type(widget) == str: # If a string is passed, this string will be logged as the answer
+#             answer = widget
+#         elif type(widget) == list: # If there are multiple widgets, record them in a list
+#             answer = []
+#             for i in range(len(widget)):
+#                 answer.append(widget[i].value)
+#         else:
+#             answer = widget.value
+
+#         # Encode user agent
+#         user_agent_hash = hashlib.md5(user_agent.encode()).hexdigest()
+        
+#         if test:
+#             url = "https://{}:{}/test/{}/{}/{}/{}/{}".format(host, port, user_agent_hash, lesson, lesson_level, question, str(answer))
+#         else:
+#             url = "https://{}:{}/{}/{}/{}/{}/{}".format(host, port, user_agent_hash, lesson, lesson_level, question, str(answer))
+            
+#         # Send_request
+#         r = requests.get(url)
+
+#         # Print widget value
+#         with output:
+#             output.clear_output()
+            
+#             if type(widget) != str:
+#                 # print("Your answer is: " + str(answer))
+#                 pass 
+            
+#             if r.status_code == requests.codes.ok:
+#                 print("Answer successfully submitted.\n")
+                
+#             out_func()
     
     button.on_click(submit)
+
